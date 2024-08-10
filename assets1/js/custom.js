705ce213f41dfa5005cba0294acef6e85cf95b89
Astr0n1 <astron0learn@gmail.com>
2024-08-10 03:31:53 +0300

    stil JS errors

--- assets1/js/custom.js
+++ assets1/js/custom.js
@@ -274,3 +274,22 @@ const Accordion = {
     Accordion.init(accordions[i]);
   }
   })();
+  // 
+  $('.owl-carousel').owlCarousel({
+    center: true,
+    loop: true,
+    margin: 30,
+    autoplay: true,
+    responsiveClass: true,
+    responsive:{
+        0:{
+            items: 2,
+        },
+        767:{
+            items: 3,
+        },
+        1200:{
+            items: 4,
+        }
+    }
+});
